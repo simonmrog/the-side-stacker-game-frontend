469657100838186b9edf0f0f381133ec4ec9bb10
simonmrog <simonmrog@gmail.com>
2023-08-29 00:17:36 -0500

    feat: adding new game and join game logic event handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,66 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import socketService from "./services/socketService";
 
+import { GameState, GameStatus } from "./interfaces/sideStacker.interface";
+import Board from "./components/Board/Board";
+
 function App() {
+  const [game, setGame] = useState<GameState | null>(null);
+
   useEffect(() => {
     socketService.connect();
 
-    socketService.on("player-connected", players => {
-      console.log("[player-connected event]: ", players);
+    socketService.on("game-created", (game: unknown) => {
+      console.log("[Event]: game-created", game);
+      setGame(game as GameState);
     });
 
-    socketService.on("message", (message: unknown) => {
-      console.log("[message event]: ", message);
-      socketService.emit("message-response", "hello");
+    socketService.on("player-joined", (game: unknown) => {
+      console.log("[Event]: user-joined");
+      setGame(game as GameState);
     });
 
-    socketService.on("random-message", (message: unknown) => {
-      console.log("[random-message event]: ", message);
+    socketService.on("waiting-for-second-user", (game: unknown) => {
+      console.log("[Event]: waiting-for-second-user");
+      setGame(game as GameState);
     });
 
     return () => {
       socketService.disconnect();
-      socketService.off("player-connected");
-      socketService.off("message");
-      socketService.off("random-message");
+      socketService.off("game-created");
+      socketService.off("player-joined");
+      socketService.off("waiting-for-second-user");
     };
   }, []);
 
-  return <div className="App">The Side Stacker Game</div>;
+  const newGame = () => socketService.emit("new-game");
+  const joinGame = () => socketService.emit("join-game");
+
+  const canJoinGame = () =>
+    game?.status === GameStatus.WAITING_FOR_SECOND_USER && !game.players.includes(socketService.getId());
+
+  const waitingForUser = () =>
+    game?.status === GameStatus.WAITING_FOR_SECOND_USER && game.players.includes(socketService.getId());
+
+  return (
+    <div className="App">
+      <h1>The Side Stacker Game</h1>
+      {!game && <button onClick={newGame}>New Game</button>}
+
+      {canJoinGame() && <button onClick={joinGame}>Join Game</button>}
+
+      {waitingForUser() && <div>Waiting for someone to join the game</div>}
+
+      {game?.status === GameStatus.STARTED && (
+        <>
+          <Board board={game.board} />
+          <button onClick={newGame}>Restart</button>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
