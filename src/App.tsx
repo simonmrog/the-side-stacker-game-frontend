01ca7efd5ca1cf373d459ed340232ccc7e20cfbd
simonmrog <simonmrog@gmail.com>
2023-08-30 18:53:56 -0500

    feat: adding event handlers to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,55 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
+import { IGameState, GameStatus, IGameStateEvent } from "./interfaces/sideStacker.interface";
+import { ReducerActions } from "./contexts/gameContext.interface";
 import socketService from "./services/socketService";
-
-import { GameState, GameStatus } from "./interfaces/sideStacker.interface";
-import Board from "./components/Board/Board";
+import { useGameContext } from "./hooks/useGameContext";
+import Board from "./components/StackerBoard/SideStacker";
 
 function App() {
   const [isConnected, setIsConnected] = useState(socketService.isConnected());
-  const [game, setGame] = useState<GameState | null>(null);
+  const { gameState, dispatch } = useGameContext();
 
+  // Disconnects the socket connection
   useEffect(() => {
+    socketService.connect();
+
+    return () => {
+      socketService.disconnect();
+    };
+  }, []);
+
+  // Handles the game events
+  useEffect(() => {
+    const onGameStart = (event: string, game: unknown) => {
+      console.log(`[Event]: ${event}`, game);
+      dispatch({ type: ReducerActions.UPDATE_GAME, payload: { gameState: game as IGameState } });
+    };
+
+    const onPlayerJoined = (event: string, game: unknown) => {
+      console.log(`[Event]: ${event}`, game);
+      dispatch({ type: ReducerActions.UPDATE_GAME, payload: { gameState: game as IGameState } });
+    };
+
+    const onPlayerIdGenerated = (event: string, playerId: unknown) => {
+      console.log(`[Event]: ${event}`, playerId);
+      dispatch({ type: ReducerActions.UPDATE_PLAYER_ID, payload: { playerId: playerId as string } });
+    };
+
     const onGameUpdate = (event: string, game: unknown) => {
-      console.log(`[Event]: ${event}`);
-      setGame(game as GameState);
+      console.log(`[Event]: ${event}`, game);
+      dispatch({ type: ReducerActions.UPDATE_GAME, payload: { gameState: game as IGameState } });
+    };
+
+    const onGameFinished = (event: string, game: unknown) => {
+      console.log(`[Event]: ${event}`, game);
+    };
+
+    const onGameDisconnected = (event: string, payload: unknown) => {
+      const { playerId, gameState } = payload as IGameStateEvent;
+      console.log(`[Event]: ${event}`, gameState);
+      console.log(`${playerId} disconnected from the game`);
+      dispatch({ type: ReducerActions.UPDATE_GAME, payload: { gameState } });
     };
 
     socketService.connect();
@@ -40,10 +76,14 @@ function App() {
     });
 
     // game interactions
-    socketService.on("game-created", (game: unknown) => onGameUpdate("game-created", game));
-    socketService.on("player-joined", (game: unknown) => onGameUpdate("player-joined", game));
+    socketService.on("game-created", (game: unknown) => onGameStart("game-created", game));
+    socketService.on("game-restarted", (game: unknown) => onGameStart("game-restarted", game));
+    socketService.on("player-joined", (game: unknown) => onPlayerJoined("player-joined", game));
+    socketService.on("player-id-generated", (payload: unknown) => onPlayerIdGenerated("player-id-generated", payload));
     socketService.on("waiting-for-second-user", (game: unknown) => onGameUpdate("waiting-for-second-user", game));
-    socketService.on("game-restarted", (game: unknown) => onGameUpdate("game-restarted", game));
+    socketService.on("player-moved", (game: unknown) => onGameUpdate("player-moved", game));
+    socketService.on("game-finished", (game: unknown) => onGameFinished("game-finished", game));
+    socketService.on("game-disconnected", (payload: unknown) => onGameDisconnected("game-finished", payload));
 
     return () => {
       socketService.disconnect();
@@ -52,21 +92,30 @@ function App() {
       socketService.off("connect_failed");
       socketService.off("disconnect");
       socketService.off("game-created");
-      socketService.off("player-joined");
-      socketService.off("waiting-for-second-user");
       socketService.off("game-restarted");
+      socketService.off("player-joined");
+      socketService.off("player-id-generated");
+      socketService.off("waiting-for-second-user");
+      socketService.off("player-moved");
+      socketService.off("game-finished");
+      socketService.off("game-disconnected");
     };
   }, []);
 
-  const newGame = () => socketService.emit("new-game");
+  const newGame = () => {
+    socketService.emit("new-game");
+    socketService.emit("join-game");
+  };
   const restartGame = () => socketService.emit("restart-game");
   const joinGame = () => socketService.emit("join-game");
 
   const canJoinGame = () =>
-    game?.status === GameStatus.WAITING_FOR_SECOND_USER && !game.players.includes(socketService.getId());
+    gameState?.status === GameStatus.WAITING_FOR_SECOND_USER && !gameState.players.includes(socketService.getId());
 
   const waitingForUser = () =>
-    game?.status === GameStatus.WAITING_FOR_SECOND_USER && game.players.includes(socketService.getId());
+    gameState?.status === GameStatus.WAITING_FOR_SECOND_USER && gameState.players.includes(socketService.getId());
+
+  const gameIsOnCourse = () => gameState?.status === GameStatus.STARTED || gameState?.status === GameStatus.FINISHED;
 
   return (
     <div className="App">
@@ -74,15 +123,15 @@ function App() {
       {!isConnected && <div>Cannot connect to the server</div>}
       {isConnected && (
         <>
-          {!game && <button onClick={newGame}>New Game</button>}
+          {!gameState && <button onClick={newGame}>New Game</button>}
 
           {canJoinGame() && <button onClick={joinGame}>Join Game</button>}
 
-          {waitingForUser() && <div>Waiting for someone to join the game</div>}
+          {waitingForUser() && <div>Waiting for someone to join the game...</div>}
 
-          {game?.status === GameStatus.STARTED && (
+          {gameState && gameIsOnCourse() && (
             <>
-              <Board board={game.board} />
+              <Board board={gameState.board} />
               <button onClick={restartGame}>Restart</button>
             </>
           )}
