5a8e80994b4dabe2d337253a330145ce9a9ebb27
simonmrog <simonmrog@gmail.com>
2023-09-04 16:40:30 -0500

    chore: adding configuration values

--- src/config/config.interface.ts
+++ src/config/config.interface.ts
@@ -1,4 +1,17 @@
 export interface IConfig {
   ENV: string;
   BACKEND_URL: string;
+  STYLING: IStylingConfig;
+}
+
+export interface IStylingConfig {
+  ERROR_FONT_COLOR: string;
+  TITLE_FONT_COLOR: string;
+  BUTTON_BACKGROUND_COLOR: string;
+  BUTTON_HOVER_BACKGROUND_COLOR: string;
+  BUTTON_FONT_COLOR: string;
+  BUTTON_SHADOW_COLOR: string;
+  BUTTON_HOVER_SHADOW_COLOR: string;
+  BUTTON_DISABLED_BACKGROUND_COLOR: string;
+  BUTTON_DISABLED_SHADOW_COLOR: string;
 }
--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,17 @@ function configFactory(): IConfig {
   return {
     ENV: process.env.NODE_ENV ?? "dev",
     BACKEND_URL: process.env.REACT_APP_BACKEND_URL ?? "http://localhost:4200",
+    STYLING: {
+      ERROR_FONT_COLOR: "#f16445",
+      TITLE_FONT_COLOR: "#3972bf",
+      BUTTON_BACKGROUND_COLOR: "#7dbf5e",
+      BUTTON_HOVER_BACKGROUND_COLOR: "#f5c736",
+      BUTTON_FONT_COLOR: "#fff",
+      BUTTON_SHADOW_COLOR: "#829a61",
+      BUTTON_HOVER_SHADOW_COLOR: "#ac9f64",
+      BUTTON_DISABLED_BACKGROUND_COLOR: "#696969",
+      BUTTON_DISABLED_SHADOW_COLOR: "#a9a9a9",
+    },
   };
 }
 
