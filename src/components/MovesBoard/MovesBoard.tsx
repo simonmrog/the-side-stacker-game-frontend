47e8c9785c25d4ac05928cc473fd8cc1d13dd91d
simonmrog <simonmrog@gmail.com>
2023-09-04 21:11:00 -0500

    chore: MovesBoard component styles

--- src/components/MovesBoard/MovesBoard.tsx
+++ src/components/MovesBoard/MovesBoard.tsx
@@ -1,7 +1,18 @@
+import config from "../../config/config";
 import { useGameContext } from "../../hooks/useGameContext";
+
+import { MovesBoardStyledWrapper, MovesBoardStyledPanel } from "./styles";
 
 function MovesBoard() {
   const { gameState } = useGameContext();
+
+  const lastMoves = (): Array<string> | null | undefined => {
+    if (!gameState) return null;
+    const enoughMoves = gameState.moves.length <= config.MOVES_TO_SHOW;
+    const array = enoughMoves ? gameState.moves : gameState.moves.slice(config.MOVES_TO_SHOW);
+    console.log(array.length);
+    return array;
+  };
 
   const getPlayerColor = (move: string) => {
     const regex = new RegExp(/Player (\d+)/i);
@@ -12,20 +23,23 @@ function MovesBoard() {
   };
 
   return (
-    <div>
-      <h1>Moves</h1>
-      <div>
-        {
-          <ul>
-            {gameState?.moves.map((move, index) => (
-              <li style={{ color: getPlayerColor(move) }} key={index}>
-                {move}
-              </li>
-            ))}
-          </ul>
-        }
-      </div>
-    </div>
+    <MovesBoardStyledWrapper>
+      <p>Last Moves</p>
+      <MovesBoardStyledPanel>
+        <div>
+          {
+            <ul>
+              {lastMoves &&
+                lastMoves()?.map((move, index) => (
+                  <li style={{ color: getPlayerColor(move) }} key={index}>
+                    {index + 1}. {move}
+                  </li>
+                ))}
+            </ul>
+          }
+        </div>
+      </MovesBoardStyledPanel>
+    </MovesBoardStyledWrapper>
   );
 }
 
---
+++ src/components/MovesBoard/styles.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+import config from "../../config/config";
+
+export const MovesBoardStyledWrapper = styled.div`
+  background-color: ${config.STYLING.MOVES_BOARD_BACKGROUND_COLOR};
+  border-radius: 5px;
+  padding: 10px 20px;
+  text-align: center;
+  margin: 30px 0 20px 0;
+
+  p {
+    font-size: 1.2rem;
+    font-weight: bold;
+    margin: 5px 10px 10px 10px;
+  }
+`;
+
+export const MovesBoardStyledPanel = styled.div`
+  background-color: white;
+  padding: 12px 10px;
+  border-radius: 5px;
+
+  ul {
+    list-style-type: none;
+  }
+
+  li {
+    text-align: left;
+    font-weight: bold;
+    padding: 8px;
+  }
+`;
