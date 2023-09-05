95dbc4efaf1f9912556a8025295f99efc5c79f23
simonmrog <simonmrog@gmail.com>
2023-09-04 21:03:58 -0500

    chore: styles for Player component

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import { StyledDiv } from "./styles";
 
 interface IPlayerProps {
--- src/components/Player/styles.ts
+++ src/components/Player/styles.ts
@@ -1,12 +1,15 @@
 import styled from "styled-components";
 
+import config from "../../config/config";
+
 interface StyledDivProps {
   color: string;
 }
 
 export const StyledDiv = styled.div<StyledDivProps>`
-  width: 40px;
-  height: 40px;
+  width: ${config.STYLING.PLAYER_SIZE};
+  height: ${config.STYLING.PLAYER_SIZE};
   border-radius: 50%;
   background-color: ${props => props.color};
+  box-shadow: 2px 2px 4px black;
 `;
